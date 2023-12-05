7da8bc78693a17ace5d056f19421e91a1fd7537d
Eddie Shin <eshinhw@gmail.com>
2023-12-05 09:31:03 -0500

    Add comments for code

--- js/clock.js
+++ js/clock.js
@@ -1,15 +1,18 @@
 const clock = document.querySelector("#clock");
 
 function getClock() {
-  const date = new Date();
+  // Hard way of displaying a real time clock
+  // const date = new Date();
+  // let hours = date.getHours();
+  // let minutes = date.getMinutes();
+  // let seconds = date.getSeconds().toString().padStart(2, "0");
+  // clock.innerText = `${hours - 12}:${minutes}:${seconds}`;
 
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let seconds = date.getSeconds().toString().padStart(2, "0");
-
-  clock.innerText = `${hours - 12}:${minutes}:${seconds}`;
-  // clock.innerText = new Date().toLocaleTimeString();
+  // Simple way using toLocaleTimeString()
+  clock.innerText = new Date().toLocaleTimeString();
 }
 
 getClock();
+
+// Run getClock() every 1 second to make the clock look like real time
 setInterval(getClock, 1000);
