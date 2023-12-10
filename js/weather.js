78f522c0c99f09ae7a3e088ef4935786891a40e3
Eddie Shin <eshinhw@gmail.com>
2023-12-11 05:41:21 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,16 +1,26 @@
-const weather = document.querySelector("#weather span:first-child");
-const city = document.querySelector("#weather span:last-child");
+const weatherIcon = document.querySelector("#weather-icon");
+const temperature = document.querySelector("#temperature");
+const city = document.querySelector("#city");
 const API_KEY = "9fa91fc9f75f05258b5ad7dfa428a3dc";
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      weather.innerText = `${data.weather[0].main}  ${data.main.temp}°C`;
+      const iconCode = data.weather[0].icon;
+
+      const temp = Math.floor(parseInt(data.main.temp));
+
+      const iconURL = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+      const imgTag = document.createElement("img");
+      imgTag.src = iconURL;
+      imgTag.style.width = "45px";
+      imgTag.style.height = "45px";
+      weatherIcon.appendChild(imgTag);
+      temperature.innerText = `${temp}°C`;
       city.innerText = data.name;
     });
 }
