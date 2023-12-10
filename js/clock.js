e97e593450eb1226ebaff0d00f1583838f51b7e3
Eddie Shin <eshinhw@gmail.com>
2023-12-11 06:14:43 +0900

    Fix margins

--- js/clock.js
+++ js/clock.js
@@ -2,14 +2,18 @@ const clock = document.querySelector("#clock");
 
 function getClock() {
   // Hard way of displaying a real time clock
-  // const date = new Date();
-  // let hours = date.getHours();
-  // let minutes = date.getMinutes();
-  // let seconds = date.getSeconds().toString().padStart(2, "0");
-  // clock.innerText = `${hours - 12}:${minutes}:${seconds}`;
+  const date = new Date();
+  let hours = date.getHours();
+  if (hours === 0) {
+    hours = 12;
+  } else if (hours > 12) {
+    hours = hours - 12;
+  } else {
+    hours = hours.toString().padStart(2, "0");
+  }
+  let minutes = date.getMinutes().toString().padStart(2, "0");
 
-  // Simple way using toLocaleTimeString()
-  clock.innerText = new Date().toLocaleTimeString();
+  clock.innerText = `${hours}:${minutes}`;
 }
 
 getClock();
